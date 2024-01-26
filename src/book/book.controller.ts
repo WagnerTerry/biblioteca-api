7698006f3611230c3e1bb0bf48720914ee4f537f
Wagner Terry <goncalveswagner15@gmail.com>
2024-01-26 16:13:15 -0300

    feat: implementando busca

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('books')
 export class BookController {
+  @Get()
+  async showBooks() {
+    return { books: [] };
+  }
+
+  @Get(':id')
+  async findOneBook(@Param() params) {
+    return { book: {}, params };
+  }
+
   @Post()
   async create(@Body() body) {
     return { body };
