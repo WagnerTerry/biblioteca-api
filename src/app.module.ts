8e8c78bf3f38bbca503b7db6d7059308f8b4d621
Wagner Terry <goncalveswagner15@gmail.com>
2024-01-28 11:09:40 -0300

    feat: criando rota para usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
 import { HealthModule } from './health/health.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [BookModule, HealthModule],
+  imports: [BookModule, UserModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
   exports: [AppService],
