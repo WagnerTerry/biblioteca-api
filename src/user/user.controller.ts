b69143c5fddeb2c5b5356c71cdcb11a0ca7447c5
Wagner Terry <goncalveswagner15@gmail.com>
2024-01-28 11:24:57 -0300

    feat: validação de user usando DTO

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,10 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
   @Post()
-  async createUser(@Body() body) {
-    return body;
+  async createUser(@Body() { email, name, password }: CreateUserDTO) {
+    return { email, name, password };
   }
 }
