324ec5e9ebea7ae18396d43b0e8dd3998ea839c1
Wagner Terry <goncalveswagner15@gmail.com>
2024-01-28 12:14:30 -0300

    feat: deletando usuario

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UpdateUserDTO } from './dto/update-user.dto';
 
@@ -12,8 +20,13 @@ export class UserController {
   @Patch(':id')
   async updateUser(
     @Body() { email, name, password }: UpdateUserDTO,
-    @Param() params,
+    @Param('id', ParseIntPipe) id: number,
   ) {
-    return { email, name, password, params };
+    return { email, name, password, id };
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return { id };
   }
 }
